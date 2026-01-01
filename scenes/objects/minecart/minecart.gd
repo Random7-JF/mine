f7d4cc7800946ec19928cef1fea8c344965f099d
Jon Fingas <jon.fingas@gmail.com>
2026-01-01 17:18:40 -0600

    path finding based on X is working now

--- scenes/objects/minecart/minecart.gd
+++ scenes/objects/minecart/minecart.gd
@@ -15,9 +15,7 @@ var debug_tilemap: TileMapLayer
 var start_pos: Marker2D
 var end_pos: Marker2D
 
-var path: Array[Vector2i] = []
-var path_index: int = 0
-var tracks: Array[Vector2i] = []
+var all_paths: Array = []
 
 func _ready() -> void:
 	track_tilemap = get_tree().get_first_node_in_group("minecart_tracks")
@@ -30,54 +28,64 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	pass
+	
+
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		debug_tilemap.clear()
-		var neighbour_tracks: Array[Vector2i] = find_track_path()
-		for track in neighbour_tracks:
+		var path: Array[Vector2i]
+		path.append(track_tilemap.local_to_map(start_pos.position))
+		create_paths(track_tilemap.local_to_map(start_pos.position), track_tilemap.get_used_cells(), path)
+		var tracks: Array[Vector2i] = find_best_path()
+		print("Best Path: ", tracks)
+		for track in tracks:
 			debug_tilemap.set_cell(track,2,Vector2i(1,0))
-		#debug_tilemap.set_cell(neighbour_tracks.back(), 2, Vector2i(0,0))
+		debug_tilemap.set_cell(tracks.back(), 2, Vector2i(0,0))
 
+# Find all neighbours of the current track tile
+	# For each neighbour
+		# create a path
+		# find the neighbours RECURSIVE
+			# pick the best neighbour
+				# add to path
+	# look at all the paths and pick the closest to the end pos.
 
-func find_track_path()-> Array[Vector2i]:
-	var current_tracks: Array[Vector2i] = track_tilemap.get_used_cells()
-	var current_tile = track_tilemap.local_to_map(position)
+func find_best_path() -> Array[Vector2i]:
+	var best_path: Array[Vector2i] = []
+	var distance: int = 2000
+	var count: int = 0
+	for path in all_paths:
+		var current_distance: int = abs(path.back().x - end_pos.position.x)
+		print("Path ", count, ":", path, " | Distance: ", current_distance, " | Current closest Distance: ", distance)
+		if  current_distance <= distance:
+			distance = current_distance
+			best_path = path
+		count +=1
+	return best_path
 	
-	var stack: Array[Vector2i]
-	var checked: Array[Vector2i]
-	var path: Array[Vector2i]
-	
-	stack.append(current_tile)
-	checked.append(current_tile)
-	
-	while stack.size() > 0:
-		var tile = stack.pop_back()
-		print("Tile: ", tile)
-		
-		var new_neighbours = find_track_neighbours(tile, current_tracks)
-		var possible_paths: Array[Vector2i]
-		var distance: float = 999
-		var next_step: Vector2i
-		for n in new_neighbours:
-			if not checked.has(n):
-				possible_paths.append(n)
-				checked.append(n)
-				print("Tile possible path added: ", n)
-				for x in possible_paths:
-					if end_pos.position.distance_to(track_tilemap.map_to_local(x)) < distance:
-						next_step = x
-						print("Next step is set to: ", x)
-				print("adding next step: ", next_step)
-				stack.append(next_step)
-				path.append(next_step)
-	print("returning path: ", path)
-	return path
 
-func find_track_neighbours(tile: Vector2i, tracks: Array[Vector2i]) -> Array[Vector2i]:
-	var neighbour_tracks: Array[Vector2i] = []
+func create_paths(tile: Vector2i, tracks: Array[Vector2i], current_path: Array[Vector2i]):
+	var possible_paths = find_neighours(tile, tracks)
+	var valid_next_steps: Array[Vector2i] = []
+	
+	for path in possible_paths:
+		if not current_path.has(path):
+			valid_next_steps.append(path)
+
+	if valid_next_steps.size() == 0:
+		all_paths.append(current_path)
+		return
+	
+	for next in valid_next_steps:
+		var new_path = current_path.duplicate()
+		new_path.append(next)
+		create_paths(next, tracks, new_path)
+
+func find_neighours(tile: Vector2i, tracks: Array[Vector2i]) -> Array[Vector2i]:
+	var neighbours: Array[Vector2i] = []
 	for dir in DIRECTIONS:
 		var check_tile: Vector2i = tile + dir
 		if tracks.has(check_tile):
-			neighbour_tracks.append(check_tile)
-	return neighbour_tracks
+			neighbours.append(check_tile)
+	return neighbours
