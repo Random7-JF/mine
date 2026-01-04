decb2366688f02975b05f8804d98980e6cc50ecb
Jon Fingas <jon.fingas@gmail.com>
2026-01-03 18:03:36 -0600

    made it so clicking action buttons work too

--- ui/hud/action_bar.gd
+++ ui/hud/action_bar.gd
@@ -5,3 +5,23 @@ extends Control
 @onready var nothing_button: Button = $PanelContainer/MarginContainer/Actions/PanelContainer3/NothingButton
 
 # TODO be able to update weapon button icon
+
+var player: Player
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+
+
+func _on_track_button_pressed() -> void:
+	if player:
+		player.current_mode = Player.PLAYER_MODE.Track
+
+
+func _on_weapon_button_pressed() -> void:
+	if player:
+		player.current_mode = Player.PLAYER_MODE.Weapon
+
+
+func _on_nothing_button_pressed() -> void:
+	if player:
+		player.current_mode = Player.PLAYER_MODE.Nothing
