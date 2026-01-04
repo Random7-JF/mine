da0890cb72ce810179a9567e8d76c11c198a3ec9
Jon Fingas <jon.fingas@gmail.com>
2026-01-03 18:18:18 -0600

    synced initial action bar to player, added safeties.

--- ui/hud/action_bar.gd
+++ ui/hud/action_bar.gd
@@ -10,23 +10,29 @@ var player: Player
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
+	if player:
+		match player.current_mode:
+			Player.PLAYER_MODE.Track:
+				track_button.grab_focus()
+			Player.PLAYER_MODE.Weapon:
+				weapon_button.grab_focus()
+			Player.PLAYER_MODE.Nothing:
+				nothing_button.grab_focus()
+	else:
+		print_debug("No Player Found.")
 
 
 func _on_track_button_pressed() -> void:
 	if player:
+		track_button.grab_focus()
 		player.current_mode = Player.PLAYER_MODE.Track
-	else:
-		print_debug("No Player Found.")
-
 
 func _on_weapon_button_pressed() -> void:
 	if player:
+		weapon_button.grab_focus()
 		player.current_mode = Player.PLAYER_MODE.Weapon
-	else:
-		print_debug("No Player Found.")
 
 func _on_nothing_button_pressed() -> void:
 	if player:
 		player.current_mode = Player.PLAYER_MODE.Nothing
-	else:
-		print_debug("No Player Found.")
+		nothing_button.grab_focus()
